33eba3cc044d58ed8d974663af36f4e0b6d67c66
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-03-24 17:54:00 +0500

    Navbar component updated

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+import Navbar from "@/components/Navbar";
 import "@/styles/globals.css";
 // import "@/styles/style.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+    </>
+  );
 }
