f59566f5213cedd53c8d62b0bda1dca18cfb49c5
Muhammad Bilal <bilalmuhammad266@yahoo.com>
2023-03-28 12:45:09 +0500

    Using the api endpoints

--- pages/blog.js
+++ pages/blog.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "@/styles/Blog.module.css";
 import Link from "next/link";
 
 const Blog = () => {
+  const [blogs, setBlogs] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/api/blogs")
+      .then((a) => {
+        return a.json();
+      })
+      .then((data) => {
+        console.log(data, "data");
+        setBlogs(data.data);
+      });
+  }, []);
+
   return (
     <>
       <div className={styles.container}>
         <main className={styles.main}>
           <h2>Popular Blogs</h2>
-          <div className={styles.blogItem}>
+          {blogs.map((item) => {
+            return (
+              <>
+                <div className={styles.blogItem} key={item.slug}>
+                  <Link href={`/blogpost/${item.slug}`}>
+                    <h3>{item.title}</h3>
+                  </Link>
+                  <p className={styles.blogItemPara}>
+                    {item.content.substring(0, 140)} .....
+                  </p>
+                </div>
+              </>
+            );
+          })}
+          {/* <div className={styles.blogItem}>
             <Link href={"/blogpost/learn-javascript"}>
               <h3>How to learn javascripy in 2022?</h3>
             </Link>
@@ -30,7 +56,7 @@ const Blog = () => {
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
             </p>
-          </div>
+          </div> */}
         </main>
       </div>
     </>
